2a65f8f8234d36ae03ad16a45be0376456c84333
Ptikiki <alicia.baudry.31@gmail.com>
2017-11-05 16:32:17 +0100

    add chordsDatas.js and update Chords Class for dynamic datas, associate piano sounds to computer keys, play song when chord found

--- src/javascript/classes/Chords.class.js
+++ src/javascript/classes/Chords.class.js
@@ -1,13 +1,18 @@
+import chordsDatas from '../datas/chordsDatas.js'
+
 class Chords {
 
     constructor(options) {
       STORAGE.chordsClass = this
-      this.chordsTab = [
+      /*this.chordsTab = [
+        // Orelsan ==> Grands Parents ==> mi, sol#, si // E5, G5#, B5
         [ 'm', 'j', 'c'],
-        [ 'x', 'a', 's']
-      ]
+        [ 'x', 'u', 's']
+      ]*/
+
       this.chordIndexInProgress = null
       this.chordTabInProgress = []
+
       this.bind()
     }
 
@@ -18,8 +23,65 @@ class Chords {
     }
 
     handleKeydown(event) {
-      STORAGE.chordsClass.chordsTab.forEach((chord, index) => {
-        if (STORAGE.chordsClass.chordIndexInProgress === index) {
+
+      for (let i = 0; i < chordsDatas.chords.length; i++ ) {
+
+        // PLAYING NOTES // mieux ici ou à dupliquer dans les deux if-for ?
+        for (let j = 0; j < chordsDatas.chords[i].notes.length; j++ ) {
+          if (event.key === chordsDatas.chords[i].notes[j].key) {
+            let noteToPlay = new Audio(chordsDatas.chords[i].notes[j].src)
+            noteToPlay.play()
+          }
+        }
+
+        if (STORAGE.chordsClass.chordIndexInProgress === null) {
+          for (let j = 0; j < chordsDatas.chords[i].notes.length; j++ ) {
+            if (event.key === chordsDatas.chords[i].notes[j].key) {
+              STORAGE.chordsClass.chordIndexInProgress = i
+            }
+          }
+        }
+        else if (STORAGE.chordsClass.chordIndexInProgress === i) {
+          let noteIsOk = false
+          for (let j = 0; j < chordsDatas.chords[i].notes.length; j++ ) {
+            if (event.key === chordsDatas.chords[i].notes[j].key) {
+              noteIsOk = true
+              if (STORAGE.chordsClass.chordTabInProgress.indexOf(chordsDatas.chords[i].notes[j].key) === -1) {
+                STORAGE.chordsClass.chordTabInProgress.push(chordsDatas.chords[i].notes[j].key)
+                if (STORAGE.chordsClass.chordTabInProgress.length === 3) {
+                  console.log('GAGNE')
+                  let songToPlay = new Audio(chordsDatas.chords[i].song)
+                  songToPlay.play()
+                  window.removeEventListener('keydown', STORAGE.chordsClass.handleKeydown)
+                  window.removeEventListener('keyup', STORAGE.chordsClass.handleKeyup)
+                  setTimeout(function() {
+                    window.addEventListener('keydown', STORAGE.chordsClass.handleKeydown)
+                    window.addEventListener('keyup', STORAGE.chordsClass.handleKeyup)
+                  }, 1000)
+                }
+              }
+            }
+          }
+          if (noteIsOk) {
+            return
+          } else {
+            console.log("PERDU")
+            STORAGE.chordsClass.chordIndexInProgress = null
+            STORAGE.chordsClass.chordTabInProgress = []
+          }
+        }
+      }
+      
+/*      STORAGE.chordsClass.chordsTab.forEach((chord, index) => {
+        if (STORAGE.chordsClass.chordIndexInProgress === null) {
+          chord.forEach((note) => {
+            if (event.key === note) {
+              console.log(event.key, note, index)
+              STORAGE.chordsClass.chordIndexInProgress = index
+            }
+          })
+        }
+        else if (STORAGE.chordsClass.chordIndexInProgress === index) {
           let noteIsOk = false
           chord.forEach((note) => {
             if (event.key === note) {
@@ -28,12 +90,14 @@ class Chords {
                 STORAGE.chordsClass.chordTabInProgress.push(note)
                 if (STORAGE.chordsClass.chordTabInProgress.length === 3) {
                   console.log('OKKKKKKKKKKK')
+                  // let songToPlay = new Audio(chordsDatas.chords[i].song)
+                  // songToPlay.play()
                   window.removeEventListener('keydown', STORAGE.chordsClass.handleKeydown)
                   window.removeEventListener('keyup', STORAGE.chordsClass.handleKeyup)
                   setTimeout(function() {
                     window.addEventListener('keydown', STORAGE.chordsClass.handleKeydown)
                     window.addEventListener('keyup', STORAGE.chordsClass.handleKeyup)
-                  }, 10000)
+                  }, 1000)
                 }
               }
               console.log(event.key, note, index, 'HERE', STORAGE.chordsClass.chordTabInProgress)
@@ -45,18 +109,13 @@ class Chords {
             STORAGE.chordsClass.chordIndexInProgress = null
             STORAGE.chordsClass.chordTabInProgress = []
           }
-        } else if (STORAGE.chordsClass.chordIndexInProgress === null) {
-          chord.forEach((note) => {
-            if (event.key === note) {
-              console.log(event.key, note, index)
-              STORAGE.chordsClass.chordIndexInProgress = index
-            }
-          })
-        }
-      })
+        } 
+      })*/
+
     }
 
     handleKeyup(event) {
+      console.log("PERDU")
       STORAGE.chordsClass.chordIndexInProgress = null
       STORAGE.chordsClass.chordTabInProgress = []
     }
---
+++ src/javascript/datas/chordsDatas.js
@@ -0,0 +1,57 @@
+const chordsDatas = {
+  chords: [
+    {
+      notes : [
+        {
+          key: 'm',
+          src: 'assets/notes/e5.wav'
+        },
+        {
+          key: 'j',
+          src: 'assets/notes/g5d.wav'
+        },
+        {
+          key: 'c',
+          src: 'assets/notes/b5.wav'
+        },
+      ],
+      song : 'assets/songs/orelsan.mp3'
+    },
+    {
+      notes : [
+        {
+          key: 'x',
+          src: 'assets/notes/e5.wav'
+        },
+        {
+          key: 'u',
+          src: 'assets/notes/g5d.wav'
+        },
+        {
+          key: 's',
+          src: 'assets/notes/b5.wav'
+        },
+      ],
+      song : 'assets/songs/orelsan.mp3'
+    },
+    {
+      notes : [
+        {
+          key: 'g',
+          src: 'assets/notes/e5.wav'
+        },
+        {
+          key: 'q',
+          src: 'assets/notes/g5d.wav'
+        },
+        {
+          key: 'n',
+          src: 'assets/notes/b5.wav'
+        },
+      ],
+      song : 'assets/songs/orelsan.mp3'
+    },
+  ]
+}
+
+export default chordsDatas
