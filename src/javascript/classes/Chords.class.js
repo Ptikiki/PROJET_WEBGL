c81592bf5bd3343898abe8ccb747cfcdcfed9a07
GUYON Mélina <mguyon@g-a106-23.local>
2017-11-06 11:23:24 +0100

    Change

--- src/javascript/classes/Chords.class.js
+++ src/javascript/classes/Chords.class.js
@@ -4,15 +4,9 @@ class Chords {
 
     constructor(options) {
       STORAGE.chordsClass = this
-      /*this.chordsTab = [
-        // Orelsan ==> Grands Parents ==> mi, sol#, si // E5, G5#, B5
-        [ 'm', 'j', 'c'],
-        [ 'x', 'u', 's']
-      ]*/
-
       this.chordIndexInProgress = null
       this.chordTabInProgress = []
-
+    
       this.bind()
     }
 
@@ -23,17 +17,9 @@ class Chords {
     }
 
     handleKeydown(event) {
+      console.log(event.keyCode)
 
       for (let i = 0; i < chordsDatas.chords.length; i++ ) {
-
-        // PLAYING NOTES // mieux ici ou à dupliquer dans les deux if-for ?
-        for (let j = 0; j < chordsDatas.chords[i].notes.length; j++ ) {
-          if (event.key === chordsDatas.chords[i].notes[j].key) {
-            let noteToPlay = new Audio(chordsDatas.chords[i].notes[j].src)
-            noteToPlay.play()
-          }
-        }
-
         if (STORAGE.chordsClass.chordIndexInProgress === null) {
           for (let j = 0; j < chordsDatas.chords[i].notes.length; j++ ) {
             if (event.key === chordsDatas.chords[i].notes[j].key) {
@@ -48,6 +34,15 @@ class Chords {
               noteIsOk = true
               if (STORAGE.chordsClass.chordTabInProgress.indexOf(chordsDatas.chords[i].notes[j].key) === -1) {
                 STORAGE.chordsClass.chordTabInProgress.push(chordsDatas.chords[i].notes[j].key)
+                let noteToPlay = new Audio(chordsDatas.chords[i].notes[j].src)
+                noteToPlay.play()
+                console.log('jajoute', chordsDatas.chords[i].notes[j].key, STORAGE.chordsClass.chordTabInProgress)
+                window.removeEventListener('keydown', STORAGE.chordsClass.handleKeydown)
+                window.removeEventListener('keyup', STORAGE.chordsClass.handleKeyup)
+                setTimeout(function() {
+                  window.addEventListener('keydown', STORAGE.chordsClass.handleKeydown)
+                  window.addEventListener('keyup', STORAGE.chordsClass.handleKeyup)
+                }, 100)
                 if (STORAGE.chordsClass.chordTabInProgress.length === 3) {
                   console.log('GAGNE')
                   let songToPlay = new Audio(chordsDatas.chords[i].song)
@@ -71,47 +66,6 @@ class Chords {
           }
         }
       }
-      
-/*      STORAGE.chordsClass.chordsTab.forEach((chord, index) => {
-        if (STORAGE.chordsClass.chordIndexInProgress === null) {
-          chord.forEach((note) => {
-            if (event.key === note) {
-              console.log(event.key, note, index)
-              STORAGE.chordsClass.chordIndexInProgress = index
-            }
-          })
-        }
-        else if (STORAGE.chordsClass.chordIndexInProgress === index) {
-          let noteIsOk = false
-          chord.forEach((note) => {
-            if (event.key === note) {
-              noteIsOk = true
-              if (STORAGE.chordsClass.chordTabInProgress.indexOf(note) === -1) {
-                STORAGE.chordsClass.chordTabInProgress.push(note)
-                if (STORAGE.chordsClass.chordTabInProgress.length === 3) {
-                  console.log('OKKKKKKKKKKK')
-                  // let songToPlay = new Audio(chordsDatas.chords[i].song)
-                  // songToPlay.play()
-                  window.removeEventListener('keydown', STORAGE.chordsClass.handleKeydown)
-                  window.removeEventListener('keyup', STORAGE.chordsClass.handleKeyup)
-                  setTimeout(function() {
-                    window.addEventListener('keydown', STORAGE.chordsClass.handleKeydown)
-                    window.addEventListener('keyup', STORAGE.chordsClass.handleKeyup)
-                  }, 1000)
-                }
-              }
-              console.log(event.key, note, index, 'HERE', STORAGE.chordsClass.chordTabInProgress)
-            }
-          })
-          if (noteIsOk) {
-            return
-          } else {
-            STORAGE.chordsClass.chordIndexInProgress = null
-            STORAGE.chordsClass.chordTabInProgress = []
-          }
-        } 
-      })*/
-
     }
 
     handleKeyup(event) {
