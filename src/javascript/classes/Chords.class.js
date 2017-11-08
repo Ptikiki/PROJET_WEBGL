4304f1ebe26995bed5a6554fa2e51406710986b3
GUYON Mélina <mguyon@g-a101-15.local>
2017-11-08 15:15:12 +0100

    Improve chords class git add .

--- src/javascript/classes/Chords.class.js
+++ src/javascript/classes/Chords.class.js
@@ -4,106 +4,96 @@ class Chords {
 
     constructor(options) {
       STORAGE.chordsClass = this
-      this.keysPressedTab = []
-      this.currentChord = null
-      this.win = false
 
-      this.prevVolumeLevel = 0.33
-      this.volumeLvl = 0.33
+      this.keyDownListener = this.handleKeydown.bind(event, this)
+      this.keyUpListener = this.handleKeyup.bind(event, this)
 
-      this.bind()
+      this.step = 0
+
+      this.enableGame()
     }
 
-    bind() {
+    enableGame() {
+      console.log('GAME ENABLE')
       let that = this
-      window.addEventListener('keydown', that.handleKeydown.bind(event, that))
-      window.addEventListener('keyup', that.handleKeyup.bind(event, that))
+      that.keysPressedTab = []
+      that.currentChord = null
+      this.win = false
+      that.step = 0
+      that.setAmbiance()
+      window.addEventListener('keydown', that.keyDownListener)
+      window.addEventListener('keyup', that.keyUpListener)
     }
 
     handleKeydown(that, event) {
-      if ( that.keysPressedTab.indexOf(event.key) === -1 && that.keysPressedTab.length < 3) {
+      if (that.keysPressedTab.indexOf(event.key) === -1 && that.keysPressedTab.length < 3) {
         that.keysPressedTab.push(event.key)
-        for (let i = 0; i < chordsDatas.chords.length; i++ ) {
-          for (let j = 0; j < chordsDatas.chords[i].notes.length; j++ ) {
-            if (event.key === chordsDatas.chords[i].notes[j].key) {
-              if (that.currentChord === null) {
-                that.currentChord = i
-              }
-              that.volumeLvl = 0
-              for (var k = 0; k < 3; k++) {
-                if( that.keysPressedTab.indexOf(chordsDatas.chords[i].notes[k].key) !== -1) {
-                  that.volumeLvl += 0.33
-                }
-              }
-              console.log(that.volumeLvl, 'SOUND LEEVL')
-              console.log(that.currentChord, 'currentChord')
-              that.testPrevVolume()
-              that.launchNote(chordsDatas.chords[i].notes[j].src)
-            }
-          }
-        }
-      }
 
-      for (let i = 0; i < chordsDatas.chords.length; i++ ) {
-        for (var j = 0; j < chordsDatas.chords[i].notes.length; j++) {
-          if (that.keysPressedTab.indexOf(chordsDatas.chords[i].notes[j].key) !== -1 && !that.win && that.currentChord === i) {
-            console.log('je suis dans accord ' , i)
-            if (that.keysPressedTab.indexOf(chordsDatas.chords[i].notes[0].key) !== -1 && that.keysPressedTab.indexOf(chordsDatas.chords[i].notes[1].key) !== -1 && that.keysPressedTab.indexOf(chordsDatas.chords[i].notes[2].key) !== -1) {
-              that.launchSound(chordsDatas.chords[i].song)
-            }
+        chordsDatas.chords.forEach((chord, index) => {
+          if (chord[0].indexOf(that.keysPressedTab[0]) !== -1) {
+            that.currentChord = index
+            that.checkCords()
           }
-        }
+        })
+
+        that.launchNote(chordsDatas.notes[event.key])
+        that.setAmbiance()
+        that.step === 3 ? that.launchSound(chordsDatas.chords[that.currentChord][2]) : ''
       }
     }
 
     handleKeyup(that, event) {
-      !that.win ? that.reset() : ''
+      console.log('PERDU')
+      window.removeEventListener('keydown', that.keyDownListener)
+      window.removeEventListener('keyup', that.keyUpListener)
+      !that.win ? that.step = 0 : ''
+      !that.win ? that.setAmbiance() : ''
+      !that.win ? setTimeout( () => { that.enableGame() }, 500) : ''
+    }
+
+    checkCords() {
+      this.step = 0
+      let numbreOfNotesOk = 0
+      this.keysPressedTab.forEach((key) => {
+        if (chordsDatas.chords[this.currentChord][0].indexOf(key) !== -1){
+          numbreOfNotesOk ++
+          this.volume += 0.33
+        }
+      })
+
+      this.keysPressedTab.length > numbreOfNotesOk ? numbreOfNotesOk = 0 : ''
+      this.step = numbreOfNotesOk
     }
 
     launchNote(note) {
+      console.log(this.step, 'STEP')
       let noteToPlay = new Audio(note)
-      noteToPlay.volume = this.volumeLvl
+      this.step === 0 ? noteToPlay.volume = 0.05 : ''
+      this.step === 1 ? noteToPlay.volume = 0.10 : ''
+      this.step === 2 ? noteToPlay.volume = 0.25 : ''
+      this.step === 3 ? noteToPlay.volume = 0.60 : ''
       noteToPlay.play()
     }
 
     launchSound(song) {
-      console.log('GAGNE')
+      console.log('GAGNE', song)
       this.win = true
 
       let songToPlay = new Audio(song)
       songToPlay.play()
 
-      window.removeEventListener('keydown', this.handleKeydown)
-      window.removeEventListener('keyup', this.handleKeyup)
-
-      setTimeout( () => {
-        window.addEventListener('keydown', this.handleKeydown.bind(event, this))
-        window.addEventListener('keyup', this.handleKeyup.bind(event, this))
-        this.win = false
-        this.reset(true)
-      }, 3000)
+      window.removeEventListener('keydown', this.keyDownListener)
+      window.removeEventListener('keyup', this.keyDownListener)
+      setTimeout( () => { this.enableGame() }, 10000)
     }
 
-    testPrevVolume() {
-      if (this.prevVolumeLevel > this.volumeLvl) {
-        !this.win ? this.reset() : ''
-      } else if (this.prevVolumeLevel < this.volumeLvl) {
-        this.volumeLvl === 0.66 ? STORAGE.renderer.setClearColor( 0x9fcec2, 1) : ''
-        this.volumeLvl === 0.99 ? STORAGE.renderer.setClearColor( 0x47d1ae, 1) : ''
-      } else if (this.prevVolumeLevel === this.volumeLvl) {
-        this.volumeLvl === 0.33 ? STORAGE.renderer.setClearColor( 0xf7f7f9, 1) : ''
-      }
-      this.prevVolumeLevel = this.volumeLvl
+    setAmbiance() {
+      this.step === 0 ? STORAGE.renderer.setClearColor( 0xfcfcfc, 1) : ''
+      this.step === 1 ? STORAGE.renderer.setClearColor( 0xb5e2d7, 1) : ''
+      this.step === 2 ? STORAGE.renderer.setClearColor( 0x65c6ae, 1) : ''
+      this.step === 3 ? STORAGE.renderer.setClearColor( 0x47d1ae, 1) : ''
     }
 
-    reset(noReset) {
-      console.log('PERDU')
-      this.keysPressedTab = []
-      this.currentChord = null
-      this.prevVolumeLevel = 0.33
-      this.volumeLvl = 0.33
-      noReset ? '' : STORAGE.renderer.setClearColor( 0xfcfcfc, 1)
-    }
 }
 
 export default Chords
---
+++ src/javascript/datas/chordsDatasSAVE.js
@@ -0,0 +1,59 @@
+const chordsDatas = {
+  chords: [
+    {
+      notes : [
+        {
+          key: 'm',
+          src: 'assets/notes/e5.wav'
+        },
+        {
+          key: 'j',
+          src: 'assets/notes/g5d.wav'
+        },
+        {
+          key: 'c',
+          src: 'assets/notes/b5.wav'
+        },
+      ],
+      song : 'assets/songs/orelsan.mp3',
+      colorStep1 : 0x9fcec2,
+      colorStep2 : 0x47d1ae
+    },
+    {
+      notes : [
+        {
+          key: 'x',
+          src: 'assets/notes/e5.wav'
+        },
+        {
+          key: 'u',
+          src: 'assets/notes/g5d.wav'
+        },
+        {
+          key: 's',
+          src: 'assets/notes/b5.wav'
+        },
+      ],
+      song : 'assets/songs/orelsan.mp3'
+    },
+    {
+      notes : [
+        {
+          key: 'g',
+          src: 'assets/notes/e5.wav'
+        },
+        {
+          key: 'q',
+          src: 'assets/notes/g5d.wav'
+        },
+        {
+          key: 'n',
+          src: 'assets/notes/b5.wav'
+        },
+      ],
+      song : 'assets/songs/orelsan.mp3'
+    },
+  ]
+}
+
+export default chordsDatas
